198eb44375d085145848d3b030bbaadd48aca0fa
Csky_OS <charlie.bitsky@gmail.com>
2025-01-02 14:52:34 -0600

    Added rtt-target and gdb debugging

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,20 @@
 #![no_std]
 
 use panic_halt as _;
+
+use cortex_m::asm::nop;
 use cortex_m_rt::entry;
+use rtt_target::{rtt_init_print, rprintln};
 
 #[entry]
 fn main() -> ! {
-    loop {}
+    rtt_init_print!();
+    rprintln!("Hello, world!");
+
+    loop {
+        rprintln!("Echo...");
+        for _ in 0..100_000 {
+            nop();
+        }
+    }
 }
